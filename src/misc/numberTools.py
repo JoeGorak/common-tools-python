fa44d8157a7b0e0bc1e3adf85e81eba464672bc8
Anuvrat Singh <anuvrat@amazon.com>
2013-10-16 16:20:50 +0530

    Add methods to get digits from a number and a number from a list of digits.

--- src/misc/numberTools.py
+++ src/misc/numberTools.py
@@ -5,8 +5,32 @@ Created on Sep 20, 2013
 '''
 
 import math
+from misc.memoize import Memoize
 
+@Memoize
 def numberOfDigitsIn(number):
     if number > 0: return int( math.log10( number ) ) + 1
     elif number == 0: return 1
     else: return int( math.log10( -number ) ) + 2
+
+@Memoize
+def getDigitsIn( number ):
+    if number < 0:
+        number *= -1
+
+    digits = []
+    while number > 0:
+        digits.append( number % 10 )
+        number = int( number / 10 )
+    digits.reverse()
+    return digits
+
+def getNumberFrom( digits ):
+    num = 0
+    for digit in digits:
+        num = num * 10 + digit
+    return num
+
+if __name__ == '__main__':
+    print( getDigitsIn( -1234 ) )
+    print( getNumberFrom( [1, 2, 3, 4] ) )
